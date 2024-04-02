c7e2d0d3fd20a79fe3094d3713566576a2138b51
robweekes <weekes.robert@gmail.com>
2024-04-02 16:56:58 -0700

    done with 6 and 7

--- problems/06-reject.js
+++ problems/06-reject.js
@@ -22,8 +22,49 @@ console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth'
 *******************************************************************************/
 
 function reject(array, cb) {
-  // Your code here 
+    let newArr = [];
+
+    for(let ele of array) {
+
+        if(cb(ele) === false) {
+            newArr.push(ele);
+            console.log(newArr);
+        }
+    }
+    console.log(newArr);
+    return newArr;
 }
+
+let isEven = function(n) {
+  return n % 2 === 0;
+};
+console.log(reject([7, 8, 5, 6, 12, 11], isEven)); // [ 7, 5, 11 ]
+
+let hasA = function(s) {
+  return s.toLowerCase().includes('a');
+};
+console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
+
+// play code
+
+let Simpsons = {
+    name: 'Homer',
+    traits: 'fat',
+    job: 'power plant',
+    key: 'you messed up'
+}
+for(let key in Simpsons) {
+    let value = Simpsons[key];
+    Simpsons['new key'] = 'more stuff'
+
+    console.log(Object.keys(Simpsons));
+    // Simpsons[new key] = not working
+    console.log(value);
+    // Simpsons['key'] = 'you messed up again'
+    console.log(key);
+    console.log(value);
+}
+
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
