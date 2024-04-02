f0a97fe630984fd873d8c1e46b591ff82f4b2101
Omega Dziko F. <megaoy@yahoo.com>
2024-04-01 21:01:11 -0400

    4th prb slvd

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -22,8 +22,34 @@ console.log(result3); // hi!!!!!
 *******************************************************************************/
 
 function multiMap(val, n, cb) {
-  // Your code here 
+
+    let result = val;
+    //loop through and stop at n
+    for (let i = 0; i < n ; i++) {
+
+    //run cb func of value
+      result = cb(result);
+
+    }
+    //return new value
+    return result;
 }
+
+
+let result1 = multiMap(7, 2, function(n) {
+  return n * 10;
+});
+console.log(result1); // 700
+
+let result2 = multiMap(7, 3, function(n) {
+    return n * 10;
+});
+console.log(result2); // 7000
+
+let result3 = multiMap("hi", 5, function(s) {
+  return s + "!";
+});
+console.log(result3); // hi!!!!!
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
